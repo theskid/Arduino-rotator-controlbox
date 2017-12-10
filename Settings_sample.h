1f557dd4499658aa0fc68e3b4c22a4a4549f51b0
Varstahl <github@underealm.com>
2017-12-10 16:17:06 +0100

    Detached basic static settings and added display enumeration

---
+++ Settings_sample.h
@@ -0,0 +1,14 @@
+// Configuration sample, rename to Settings.h and change your parameters accordingly
+//
+
+// Your QRZ
+const String QRZ = "QRZ";
+
+// Your Name
+const String NAME = "Name";
+
+// The type of display in your possession
+const DISPLAY_TYPE DisplayType = DISPLAY_TYPE::TFT_HVGA_480x320;
+
+// (Un)comment to enable/disable debug mode
+//#define DEBUG
