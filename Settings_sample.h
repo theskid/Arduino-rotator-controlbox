8ba618164982680791588989737f712ca054ff99
Diego Cioschi aka SkiD <skid.w38@gmail.com>
2017-12-14 00:04:49 +0100

    The constant that controls the overlap tolerance has been transferred to the configuration file

--- Settings_sample.h
+++ Settings_sample.h
@@ -7,6 +7,11 @@ const String QRZ = "QRZ";
 // Your Name
 const String NAME = "Name";
 
+// This number represents the amount of degrees to be performed
+// This value is an integer and has a range of 0 to 90
+// "const int overLapTollerance = 5;" indicates that the rotor stroke can be 5 degrees less than 0 and more than 5 compared to 359
+const int overLapTollerance = 15;
+
 // The type of display in your possession
 const DISPLAY_TYPE DisplayType = DISPLAY_TYPE::TFT_HVGA_480x320;
 
