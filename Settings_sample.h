3d5495755ab384290f5a5b7d71d1040d0f0ed078
Varstahl <github@underealm.com>
2017-12-14 20:54:13 +0100

    Enhanced settings sample descriptions

--- Settings_sample.h
+++ Settings_sample.h
@@ -1,6 +1,6 @@
-// Configuration sample, rename to Settings.h and change your parameters accordingly
-//
-#pragma once
+/*
+** Configuration sample, rename to Settings.h and change your parameters accordingly
+*/
 
 // Your QRZ
 #define QRZ "QRZ"
@@ -10,12 +10,23 @@
 
 //  The amount of degrees overlap allowance. Must be a number between 0 and 90
 //
-//  "OVERLAP_TOLERANCE 5" indicates that the rotor stroke can overlap 5 degrees in
+//  "OVERLAP_TOLERANCE 5" indicates that the rotor turn can overlap 5 degrees in
 //  both directions [-5..364] instead of the standard [0..359]
+//
 #define OVERLAP_TOLERANCE 15
 
-// The display model to initialize. Uncomment the one the matches your display shield
+//  IMPORTANT!
+//
+//  The display model to initialize. Uncomment the one the matches your display shield
+//
 //#define TFT_HVGA_480x320
+
+
+/*
+**  THERE BE DRAGONS
+**
+**  Only go further if you know what you're doing
+*/
 
 // (Un)comment to enable/disable debug mode
 //#define DEBUG
