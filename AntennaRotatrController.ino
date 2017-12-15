20fea47817a6e3c621a360acd91eeb6b4b3e0634
Varstahl <github@underealm.com>
2017-12-15 17:20:33 +0100

    Fixed overlap for beamDir

--- AntennaRotatrController.ino
+++ AntennaRotatrController.ino
@@ -144,7 +144,7 @@ void UserPrintAngle(const int& x, const int& y, int angle, const COLORS& color, 
         ra = COLORS::Red;
         bIsOver = true;
     }
-    if (bIsOver != bWasLastOver) {
+    if ((BHTYPE::BeamDIR == type) && (bIsOver != bWasLastOver)) {
         bWasLastOver = bIsOver;
         UserPrint(/*overlapAlert.x*/RIGHT, overlapAlert.y, ("OVER"), bIsOver ? COLORS::Red : COLORS::Black);
     }
