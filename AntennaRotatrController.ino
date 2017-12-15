3c2377b11f7306ab6632ae606102ee09599aa886
Varstahl <github@underealm.com>
2017-12-15 18:23:07 +0100

    Bandaid fix for Overlap alert Needs further investigation

--- AntennaRotatrController.ino
+++ AntennaRotatrController.ino
@@ -144,9 +144,11 @@ void UserPrintAngle(const int& x, const int& y, int angle, const COLORS& color, 
         ra = COLORS::Red;
         bIsOver = true;
     }
-    if ((BHTYPE::BeamDIR == type) && (bIsOver != bWasLastOver)) {
-        bWasLastOver = bIsOver;
-        UserPrint(/*overlapAlert.x*/RIGHT, overlapAlert.y, ("OVER"), bIsOver ? COLORS::Red : COLORS::Black);
+    if (BHTYPE::BeamDIR == type)
+        //if (bIsOver != bWasLastOver)) {
+        //    bWasLastOver = bIsOver;
+            UserPrint(/*overlapAlert.x*/RIGHT, overlapAlert.y, ("OVER"), bIsOver ? COLORS::Red : COLORS::Black);
+        //}
     }
 
     // SSF font size: 32x50 pixels
